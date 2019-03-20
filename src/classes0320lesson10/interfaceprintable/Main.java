b7da2ddbf9b2fdf96f301e693f1379badc54fa35
a-kuzmenko <QAZwsx12345-1980>
2019-03-20 20:42:49 +0200

    interface Printable

---
+++ src/classes0320lesson10/interfaceprintable/Main.java
@@ -0,0 +1,19 @@
+package classes0320lesson10.interfaceprintable;
+
+/**
+ * Created by UITS-Admin on 20.03.2019.
+ */
+public class Main {
+    public static void main(String[] args) {
+        Book b1 = new Book("Cook Book");
+        Book b2 = new Book("Pattern MVC");
+
+        Magazine m1 = new Magazine("AutoMir");
+        Magazine m2 = new Magazine("Sports Illustrated");
+        Printable[] prints = {b1, b2, m1, m2};
+
+        for(Printable item : prints){
+            item.print();
+        }
+    }
+}
