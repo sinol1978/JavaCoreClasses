2642976775df70775faac5f7cbb4228858401ef0
a-kuzmenko <QAZwsx12345-1980>
2019-03-27 20:16:44 +0200

    strings task01

--- src/classes0327/lesson12/strings/Task01strings.java
+++ src/classes0327/lesson12/strings/Task01strings.java
@@ -1,7 +1,7 @@
 package classes0327.lesson12.strings;
 
 /**
- * Created by UITS-Admin on 27.03.2019.
+ * Created by Oleg Sinelnikov 27.03.2019.
  */
 public class Task01strings {
     public static void main(String[] args) {
@@ -9,17 +9,25 @@ public class Task01strings {
         System.out.println(isStringEndsWith(str, "!!!"));
         System.out.println(isStringStartsWith(str, "I like"));
         System.out.println(isStringContains(str, "Java"));
+
+        checkString(str);
     }
 
-    private static boolean isStringEndsWith(String str1, String str2){
+    private static boolean isStringEndsWith(String str1, String str2) {
         return str1.endsWith(str2);
     }
 
-    private static boolean isStringStartsWith(String str1, String str2){
+    private static boolean isStringStartsWith(String str1, String str2) {
         return str1.startsWith(str2);
     }
 
-    private static boolean isStringContains(String str1, String str2){
+    private static boolean isStringContains(String str1, String str2) {
         return str1.contains(str2);
+    }
+
+    private static void checkString(String str) {
+        System.out.println(str.endsWith("!!!"));
+        System.out.println(str.startsWith("I like"));
+        System.out.println(str.contains("Java"));
     }
 }
