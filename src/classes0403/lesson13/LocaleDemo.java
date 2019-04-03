43db46a4ca40428c96488514a7853906004f9a4c
sinol1978 <sinol1978@gmail.com>
2019-04-03 21:26:48 +0300

    locale

--- src/classes0403/lesson13/LocaleDemo.java
+++ src/classes0403/lesson13/LocaleDemo.java
@@ -1,14 +1,50 @@
 package classes0403.lesson13;
 
 import java.io.UnsupportedEncodingException;
+import java.text.NumberFormat;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
 public class LocaleDemo {
     public static void main(String[] args) throws UnsupportedEncodingException {
-        printResourceInfo(new Locale("", ""));
+        printResourceInfo(Locale.ROOT);
         printResourceInfo(new Locale("ru", "RU"));
         printResourceInfo(new Locale("uk", "UA"));
+        double number = 123.4567;
+        Locale locFR = new Locale("fr");
+        NumberFormat numberFormat1 = NumberFormat.getInstance();
+        System.out.println("Число в текущей локали: " + numberFormat1.format(number));
+
+        NumberFormat numberFormat2 = NumberFormat.getInstance(locFR);
+        System.out.println("Число во французской локали: " + numberFormat2.format(number));
+
+        NumberFormat numberFormat3 = NumberFormat.getCurrencyInstance();
+        System.out.println("Денежная единица в текущей локали: " + numberFormat3.format(number));
+
+        NumberFormat numberFormat4 = NumberFormat.getCurrencyInstance(locFR);
+        System.out.println("Денежная единица во французской локали: " + numberFormat4.format(number));
+
+        NumberFormat numberFormat5 = NumberFormat.getPercentInstance();
+        System.out.println("Процент в текущей локали: " + numberFormat5.format(number));
+
+        NumberFormat numberFormat6 = NumberFormat.getPercentInstance(locFR);
+        System.out.println("Процент во французской локали: " + numberFormat6.format(number));
+
+        System.out.println(Locale.getDefault());
+        double d = 123.45678;
+        NumberFormat nf = NumberFormat.getInstance();
+
+        System.out.print("Максимальное количество знаков в дробной части "
+                + nf.getMaximumFractionDigits() + ": ");
+        System.out.println(nf.format(d));
+
+        nf.setMaximumFractionDigits(7);
+        System.out.print("Максимальное количество знаков в дробной части 7: ");
+        System.out.println(nf.format(d));
+
+        nf.setMaximumIntegerDigits(2);
+        System.out.print("Максимальное количество знаков в целой части 2: ");
+        System.out.println(nf.format(d));
 
     }
 
